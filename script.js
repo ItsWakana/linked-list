854c8e715baf243a2da66fc5185e565256951ce8
wakana <wakanawardrobe@gmail.com>
2022-10-19 15:38:20 +0100

    find last node and append new node to the end

--- script.js
+++ script.js
@@ -1,27 +1,39 @@
 class Node {
 
-    constructor(data = null, nextNode = null) {
+    constructor(data = null, next = null) {
         this.data = data;
-        this.nextNode = nextNode;
+        this.next = next;
     }
 
     value = (value) => {
         this.data = value;
     }
 
-    next = (node) => {
-        this.nextNode = node;
-    }
-    
-
 }
 
-
 class LinkedList {
-    
-    prepend = (value) => {
-        const node = new Node(value);
+
+    constructor() {
+        this.head = new Node('one');
     }
+    
+    append = (value) => {
+        const node = new Node(value);
+        const findEnd = (node) => {
+            if (node.next === null) {
+                return node;
+            }
+            return findEnd(node.next);
+        }
+
+        const lastNode = findEnd(this.head);
+        lastNode.next = node;
+    }   
 }
 
 const list = new LinkedList;
+
+list.append('another node');
+list.append('3rd');
+list.append('4th');
+console.log(list);
